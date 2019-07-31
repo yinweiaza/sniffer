3c0db6f6e21c09a93942280a136cacb870c199f4
yinweiaza <yinwei@2012>
2019-07-31 20:58:59 +0800

    tt

--- sniffer.py
+++ sniffer.py
@@ -5,9 +5,9 @@ import socket
 class PrepareSock(object):
     def __init__(self):
         host = socket.gethostbyname(socket.gethostname())
-        sock = socket.socket(socket.AF_INET,socket.SOCK_RAW, socket.IPPROTO_IP)     # 原始套接字;
+        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)     # 原始套接字;
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)          # 打开地址复用功能；
-        sock.bind((host, 0))                                         # 绑定地址，接口；
+        sock.bind((host, 0))                                         # 绑定地址，公共接口；
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)        # 设置数据保护IP头部
         sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)             # 开启混杂模式；
         self.sock = sock
@@ -16,7 +16,7 @@ class PrepareSock(object):
         return  self.sock
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        self.sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)           # 关闭混杂模式;
 
 
 class Sniffer(object):
@@ -44,4 +44,4 @@ class Sniffer(object):
 
 if __name__ == "__main__":
     sniffer = Sniffer()
-    sniffer.sniffer(10, 65535, True, True)
+    sniffer.sniffer(50, 65535, True, True)
